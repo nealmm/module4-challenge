fbd745e5f41a47bb3c236c400ce080367743b47a
Matt Neal <neal.matt.m@gmail.com>
2022-06-15 21:25:41 -0400

    basic js setup

---
+++ quiz.js
@@ -0,0 +1,3 @@
+document.getElementById("start-btn").addEventListener("click", () => {
+
+});
---
+++ scoreboard.js
@@ -0,0 +1,7 @@
+document.getElementById("back-btn").addEventListener("click", () => {
+
+});
+
+document.getElementById("clear-btn").addEventListener("click", () => {
+
+});
